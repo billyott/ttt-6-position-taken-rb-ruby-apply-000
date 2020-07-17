1dcd2d27dc4e9f95e0358c8da7a9dc463edcc8f4
Billy Ott <willrichott@gmail.com>
2020-07-17 18:59:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     return false
-  elsif board[index] == "X" || "O"
+  elsif board[index] == "X" or "O"
     return true
   else
     return false
